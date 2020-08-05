440b15e14524f4d17d6518ea866d524d746041db
George Stavrinos <stavrinosgeo@gmail.com>
2020-08-05 20:55:03 +0300

    A dirty and somewhat version of the mission manager

--- ros/mission_manager.py
+++ ros/mission_manager.py
@@ -2,13 +2,14 @@
 import rospy
 from roboskel_msgs.msg import Mission
 from geometry_msgs.msg import PoseStamped
-from actionlib_msgs.msg import GoalStatus
+from actionlib_msgs.msg import GoalStatusArray
 
 goal_pub = None
 status_sub = None
 mission_goals = []
 mission_stays = []
 active_goal = False
+prevt = rospy.Time(0)
 
 def sendNextGoal():
     if mission_goals:
@@ -16,35 +17,43 @@ def sendNextGoal():
 
 def missionCallback(mission):
     global mission_goals, mission_stays, status_sub
-    if mission_goals and len(mission_goals) == len(mission_stays):
+    if mission.poses and len(mission.poses) == len(mission.stay):
         print("Starting mission", mission.name)
-        mission_goals = mission.poses[:]
-        mission_stays= mission.stay[:]
-        status_sub = rospy.Subscriber("/move_base/status", GoalStatus, moveBaseStatus)
+        mission_goals = list(mission.poses[:])
+        mission_stays = list(mission.stay[:])
         sendNextGoal()
 
 def moveBaseStatus(msg):
-    global mission_goals, mission_stays
-    if msg.status == 3 and active_goal:
-        print("Reached goal!")
-        mission_goals.pop(0)
-        print("Staying for", str(mission_stays[0]), "seconds...")
-        rospy.sleep(mission_stays[0])
-        mission_stays.pop(0)
-        if not mission_goals:
-            print("Mission complete!")
-            status_sub.unregister()
-        else:
-            sendNextGoal()
-    active_goal = msg.status == 1
+    global mission_goals, mission_stays, active_goal, status_sub, prevt
+    if msg.status_list and mission_goals:
+        msg = msg.status_list[-1]
+        currt = rospy.Time.now()
+        if msg.status == 3 and currt - prevt >= rospy.Duration(3):
+            print("Reached goal!")
+            mission_goals.pop(0)
+            print("Staying for", str(mission_stays[0]), "seconds...")
+            rospy.sleep(mission_stays[0])
+            mission_stays.pop(0)
+            if not mission_goals:
+                print("Mission complete!")
+                #  status_sub.unregister()
+            else:
+                #  status_sub = rospy.Subscriber("/move_base/status", GoalStatusArray, moveBaseStatus)
+                #  rospy.sleep(2)
+                sendNextGoal()
+                prevt = currt
+        active_goal = msg.status != 3
 
 def init():
-    global goal_pub 
+    global goal_pub, status_sub
     rospy.init_node("mission_manager")
 
     goal_pub = rospy.Publisher("/move_base_simple/goal", PoseStamped, queue_size=1)
 
     sub = rospy.Subscriber("/mission_manager/mission", Mission, missionCallback)
 
+    status_sub = rospy.Subscriber("/move_base/status", GoalStatusArray, moveBaseStatus)
+
     rospy.spin()
 
+init()
