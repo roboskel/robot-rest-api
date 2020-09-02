ac1e410238930b12ed17504a8a88dc43ca5fe07c
George Stavrinos <stavrinosgeo@gmail.com>
2020-09-02 12:40:37 +0300

    Added tests for the new endpoints

---
+++ rest/tests/estop_off.sh
@@ -0,0 +1,1 @@
+curl -H 'Content-Type: application/json' --data '{"state": "off"}' http://localhost:8000/api/estop/
---
+++ rest/tests/estop_on.sh
@@ -0,0 +1,1 @@
+curl -H 'Content-Type: application/json' --data '{"state": "on"}' http://localhost:8000/api/estop/
